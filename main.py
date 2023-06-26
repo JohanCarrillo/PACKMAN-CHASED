a494e482cda5a7d952fcc85520b1a8e7ed7522eb
JohanCarrillo <johan.carrillot@gmail.com>
2023-06-26 10:13:08 -0500

    added base classes and created main function

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import pygame
 from sys import exit
+from board import board_skelleton
 
-WIDTH = 700
-HEIGHT = 700
+UNIT_SIZE = 25 # in pixel
+WIDTH = len(board_skelleton[0]) * UNIT_SIZE
+HEIGHT = len(board_skelleton) * UNIT_SIZE
+VALID_KEYS = {
+    "up": [pygame.K_w, pygame.K_UP],
+    "down": [pygame.K_s, pygame.K_DOWN],
+    "right": [pygame.K_d, pygame.K_RIGHT],
+    "left": [pygame.K_a, pygame.K_LEFT]
+}
 
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Runner')
-clock = pygame.time.Clock()
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((WIDTH, HEIGHT))
+	pygame.display.set_caption('Pack-Man Chased')
+	clock = pygame.time.Clock()
 
-# event loop
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			exit()
+	# event loop
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				exit()
 
-	pygame.display.update()
-	clock.tick(60)
+		pygame.display.update()
+		clock.tick(60)
+
+if __name__ == '__main__':
+  main()
---
+++ packman.py
@@ -0,0 +1,15 @@
+import pygame
+
+IMAGES_PATH = 'assets/packman'
+
+class Packman(pygame.sprite.Sprite):
+	def __init__(self):
+		super().__init__()
+
+		def set_draw(self):
+			pass
+
+		def update(self):
+			pass
+
+		packman_animation_1 = pygame.image.load(IMAGES_PATH)
